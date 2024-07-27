be16288f2b6f106598208f58e0f8f5cb6b7eadff
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-07-27 15:13:42 +0330

    Add some translation to app and make the page persian (not completely)

--- config/settings.py
+++ config/settings.py
@@ -234,7 +234,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'fa'
 
 # وقتی رزتا رو میذاریم همه زبان ها رو میاره. در حالی که ما افغانستانی و عربی و غیره رو نمیخوایم.
 # میتونیم این طوری فقط چیزایی که میخوایم رو بهش بگیم. با این متغیر
