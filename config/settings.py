4377730cbba406560c6b141c853f8ba651671970
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2026-01-08 10:05:27 +0330

    use postgres

--- config/settings.py
+++ config/settings.py
@@ -227,15 +227,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         # "ENGINE": "django.db.backends.mysql",
-        # "ENGINE": "django.db.backends.postgresql",
-        # "NAME": os.getenv("DJANGO_DATABASE_NAME"),
-        # "USER": os.getenv("DJANGO_DATABASE_USERNAME"),
-        # "PASSWORD": os.getenv("DJANGO_DATABASE_PASSWORD"),
-        # "HOST": "localhost",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "root",
+        "PASSWORD": "WPuuB7WdHeSpDdQacN3xZl7w",
+        "HOST": "confectionery-postgres",
         # "PORT": 3306,
-        # "PORT": 5432,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "PORT": 5432,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
