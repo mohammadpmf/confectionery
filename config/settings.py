8b8bb3f7bfdaf50d3babd127af7b09af970ad6be
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2026-01-08 10:43:19 +0330

    make debug=False

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # علامت $ هر جا بود باید یه $ دیگه کنارش بنویسیم. چون برای داکر کامپوز اسکیپ کاراکتر هست.
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
