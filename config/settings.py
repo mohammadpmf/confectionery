674eabcb707921c26e4573dbb13b8829e5d7e5ac
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2026-01-08 09:23:58 +0330

    use sqlite

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # علامت $ هر جا بود باید یه $ دیگه کنارش بنویسیم. چون برای داکر کامپوز اسکیپ کاراکتر هست.
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -226,16 +226,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        # "ENGINE": "django.db.backends.mysql",
         # "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DJANGO_DATABASE_NAME"),
-        "USER": os.getenv("DJANGO_DATABASE_USERNAME"),
-        "PASSWORD": os.getenv("DJANGO_DATABASE_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": 3306,
+        # "NAME": os.getenv("DJANGO_DATABASE_NAME"),
+        # "USER": os.getenv("DJANGO_DATABASE_USERNAME"),
+        # "PASSWORD": os.getenv("DJANGO_DATABASE_PASSWORD"),
+        # "HOST": "localhost",
+        # "PORT": 3306,
         # "PORT": 5432,
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
